073a29d171ff5bf2e15f3a8383c8d88a3c63e5ca
PC <PC@Emanuelpc>
2022-07-10 11:32:13 -0600

    Html funcional que agrega nuevos usuarios, cambios en la base de datos

--- src/main/resources/templates/AutoCR.sql
+++ src/main/resources/templates/AutoCR.sql
@@ -1,12 +1,14 @@
 CREATE SCHEMA `AutoCR`;
 
 CREATE TABLE `AutoCR`.`usuario`(
-idusuario varchar(10) not null,
+idusuario int not null auto_increment,
 nombre varchar(20) not null,
 apellidos varchar(30) not null,
 correo varchar(25) null,
+cedula varchar(10) null,
 telefono varchar(15) null,
-primary key(`idusuario`))
+contraseña varchar(15) not null,
+primary key(idusuario))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_eo_0900_ai_ci;
@@ -15,3 +17,7 @@ create user 'admin'@'%' identified by 'admin123';
 grant all privileges on AutoCR.* to 'admin'@'%';
 flush privileges;
 
+use AutoCR;
+drop table usuario;
+
+select * from usuario;
